fe865e6f16f65e7bd34a0ce868be7f4ba01b9fca
Marlarhb <165163918+marlarhb@users.noreply.github.com>
2024-08-28 16:41:20 +0200

    Create HMDDTMH.py

---
+++ HMDDTMH.py
@@ -0,0 +1,27 @@
+print("Welcome to the 'How many days does this month have' program?")
+month = input("Enter a month: ")
+
+if month == "January":
+    print("January has 31 days.")
+elif month == "February":
+    print("February has 28 days, except in leap years. Upcoming leap years are 2028, 2032.")
+elif month == "March":
+    print("March has 31 days.")
+elif month == "April":
+    print("April has 30 days.")
+elif month == "May":
+    print("May has 31 days.")
+elif month == "June":
+    print("June has 30 days.")
+elif month == "July":
+    print("July has 31 days.")
+elif month == "August":
+    print("August has 31 days.")
+elif month == "September":
+    print("September has 30 days.")
+elif month == "October":
+    print("October has 31 days.")
+elif month == "November":
+    print("November has 30 days.")
+elif month == "December":
+    print("December has 31 days.")
